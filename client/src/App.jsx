1a01f574ecadbc83e796ef92d36971a7a7868927
sarahtech2022 <106985498+sarahtech2022@users.noreply.github.com>
2023-03-31 13:43:19 -0600

    saving css changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,7 +10,9 @@ function App() {
     <div className="App">
       <h2 id="title">City Weather Search </h2>
       <img id="imagetitle" src='https://cdn-icons-png.flaticon.com/512/9985/9985865.png'></img>
+      <img id="imagetitle3" src='https://cdn-icons-png.flaticon.com/512/9985/9985964.png'></img>
       <img id="imagetitle2" src='https://cdn-icons-png.flaticon.com/512/3767/3767036.png'></img>
+      
       <ListCities />
 
     </div>
